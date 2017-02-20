512bec637662f2c6ce20711777ae72aa199dfa34
Haseko <valerii777999@gmail.com>
2017-02-20 16:12:23 +0900

    Delau community

--- www/js/page_community.js
+++ www/js/page_community.js
@@ -8,10 +8,11 @@ var community = {
 		var ifClear = true;
 		function createGroup(value) {
 			var block;
-			block = $("<div class='community__group' data-id='"+value.id+"'><img src='"+value.icon+"' /><div class='community__name'>"+value.name+"</div><div class='post__group__users'>"+value.users.length+"</div></div>");
+			block = $("<div class='community__group' data-id='"+value.id+"'><img src='"+value.icon+"' /><div class='community__name'>"+value.name+"</div><div class='post__group__users'>"+JSON.parse(value.users).length+"</div></div>");
 			$(".communityMain").append(block);
 			ifClear = false;
 		}
+		
 		for(var i = 0; i < DataAjax.grouplist.length; i++) {
 			createGroup(DataAjax.grouplist[i]);
 		}
@@ -20,41 +21,17 @@ var community = {
 			app.alert(dictionary.noContent, dictionary.sorry);
 		}
 		
-		news.bindEvents();
+		community.bindEvents();
 	},
 	bindEvents: function(){
-		/*$('.category__list__inPicker').on('click', '.newsCategory', function (e) {
-			var categ = $(this).attr('data-newscat');
-			$('.newsMain').attr("data-category",categ);
-			app.closeModal('.categoryPicker');
-			news.render();			
-		});
-		$('.newsMain').on('click', '.post', function (e) {
+		$('.communityMain').on('click', '.community__group', function (e) {
+			/*var postId = $(this).attr('data-id');
+			$('.community__fullPost').attr("data-id",postId);
+			mainView.router.load({pageName: 'community__full__post'});*/
 			var postId = $(this).attr('data-id');
-			$('.fullPost').attr("data-id",postId);
-			mainView.router.load({pageName: 'news__full__post'});
+			$('.community__fullPost').attr("data-id",postId);
+			mainView.router.load({pageName: 'community__full__post'});
 		});
-
-		if(!news.notFirstUse) {
-			$('.open-categoryPicker').on('click', function () {
-				app.pickerModal('.categoryPicker');
-			});
-			$('.newNews').on('click', function () {
-				$('.newsMainTriggerScroll').scrollTop(0);
-				$('.newNews').removeClass('state_active');
-				news.activeScroll = false;
-			});
-			$('.newsMainTriggerScroll').on('scroll', function () {
-				if(news.activeScroll) {
-					if($('.newsMainTriggerScroll').scrollTop() <80) {		
-						$('.newNews').removeClass('state_active');
-						news.activeScroll = false;
-					}
-				}
-			});
-		}
-		news.notFirstUse = true;
-		*/
 	},
 	notFirstUse: false,
 }
