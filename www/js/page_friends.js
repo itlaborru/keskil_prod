194a6f17dcd6709e4162ff173da58590c5fc21f0
Руслан Елизаров < >
2017-02-22 16:48:17 +0900

    friends and bugs

--- www/js/page_friends.js
+++ www/js/page_friends.js
@@ -3,24 +3,45 @@ var friends = {
 		
 	},
 	fgh: function() {
-		var newPageContent = "<div class='page' data-page='friends'>"+friends.content+"<div class='page-content friendList'> </div></div>";
-		mainView.router.loadContent(newPageContent);
-		console.log("asdasd");
+		//friends.content = "<div class='page' data-page='friends'>"+friends.content+"<div class='page-content friendList'></div></div>";
+		friends.content = '<div class="page" data-page="friends">' +
+                        '<div class="page-content friendList">' +
+                          friends.content +
+                        '</div>' +
+                      '</div>';
+		mainView.router.loadContent(friends.content);
+		
 	},
 	suc: function(datas){
 		data = JSON.parse(datas);
 		var block;
+		friends.lastfriend = friends.lastfriend-1;
 		block ="<div class='friendinfo userHref' user-id='"+data.id+"' style='margin: 0 auto 50px; max-height: 200px; max-width: 200px;'><img class='user__icon"+data.id+"' src='"+data.icon+"' style='max-height: 200px; max-width: 200px; background-size: contain; margin: auto;'/><div class='user__fullname"+data.id+"'>"+data.lname+" "+data.fname+" "+data.mname+"</div><div class='user__login"+data.id+"'>"+data.login+"</div></div>";
 		friends.content = friends.content+block+"";
-		friends.fgh();
-		
+		if(friends.lastfriend == 0) {
+			friends.fgh();
+			console.log("heh")
+		}
 	},
 	makingNewPage: function(userId){
+		friends.content = '';
+		friends.friendList ='';
 		
-		$.each(userInfo.friends, function(i, val) {
-			var data = {'type': 'getUserInfo','id':val};
-			ajax(entrypoints.newUserInfo.url,data,friends.suc);
-		});
+		var data = {'type': 'getUserInfo','id':userId};
+			ajax(entrypoints.newUserInfo.url,data,function(datas) {
+				data = JSON.parse(datas);
+				friends.friendList = data.friends
+				friends.lastfriend = data.friends.length;
+				console.log(friends.friendList);
+				$.each(friends.friendList , function(i, val) {
+					var data = {'type': 'getUserInfo','id':val};
+					ajax(entrypoints.newUserInfo.url,data,friends.suc);
+				});
+			});
+		
+		
 	},
+	friendList: '',
 	content: '',
+	lastfriend:0 ,
 }
--- www/js/page_userPage.js
+++ www/js/page_userPage.js
@@ -16,7 +16,7 @@ var userPage = {
 	makingNewPage: function(userId){
 		var newPageContent = '<div class="page" data-page="userPage">' +
                         '<div class="page-content">' +
-                          '<div style="margin: 0 auto 50px; max-height: 200px; max-width: 200px;"><img src=""class="user__icon" style="max-height: 200px; max-width: 200px; background-size: contain; margin: auto;"/></div><div class="user__fullname"></div><div class="user__name"></div><div class="user__login"></div>' +
+                          '<div style="margin: 0 auto 50px; max-height: 200px; max-width: 200px;"><img src=""class="user__icon" style="max-height: 200px; max-width: 200px; background-size: contain; margin: auto;"/></div><div class="user__fullname"></div><div class="user__name"></div><div class="user__login"></div>' +'<li><a href="#friends" class="index-links__link userFriendsHref " style="display: block;"user-id ="'+userId+'" ><span class="index-links__text">друзья</span></a></li>'
                         '</div>' +
                       '</div>';
 		mainView.router.loadContent(newPageContent);
