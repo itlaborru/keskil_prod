1117df38a23bfc297f389da64f8153f04ad427b4
Руслан Елизаров < >
2017-03-05 13:59:21 +0900

    rebuild of friends

--- www/js/page_friends.js
+++ www/js/page_friends.js
@@ -4,12 +4,7 @@ var friends = {
 	},
 	fgh: function() {
 		//friends.content = "<div class='page' data-page='friends'>"+friends.content+"<div class='page-content friendList'></div></div>";
-		friends.content = '<div class="page" data-page="friends">' +
-                        '<div class="page-content friendList">' +
-                          friends.content +
-                        '</div>' +
-                      '</div>';
-		mainView.router.loadContent(friends.content);
+		
 		
 	},
 	suc: function(datas){
@@ -19,16 +14,21 @@ var friends = {
 		block ="<div class='friendinfo userHref' user-id='"+data.id+"' style='margin: 0 auto 50px; max-height: 200px; max-width: 200px;'><img class='user__icon"+data.id+"' src='"+data.icon+"' style='max-height: 200px; max-width: 200px; background-size: contain; margin: auto;'/><div class='user__fullname"+data.id+"'>"+data.lname+" "+data.fname+" "+data.mname+"</div><div class='user__login"+data.id+"'>"+data.login+"</div></div>";
 		friends.content = friends.content+block+"";
 		if(friends.lastfriend == 0) {
-			friends.fgh();
-			console.log("heh")
+			$('.friendList').html(block);
 		}
 	},
 	makingNewPage: function(userId){
 		friends.content = '';
-		friends.friendList ='';
+		friends.friendList = '';
+		var friendsContent = '<div class="page" data-page="friends">' +
+                        '<div class="page-content friendList">' +
+                        '</div>' +
+                      '</div>';
+		mainView.router.loadContent(friendsContent);
 		
 		var data = {'type': 'getUserInfo','id':userId};
 			ajax(entrypoints.newUserInfo.url,data,function(datas) {
+				console.log(datas);
 				data = JSON.parse(datas);
 				friends.friendList = data.friends
 				friends.lastfriend = data.friends.length;
